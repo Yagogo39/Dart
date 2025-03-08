075e6fb6d9d4bc22d917dba9f8d70dedee27d684
Yagogoo39 <118009609+Yagogo39@users.noreply.github.com>
2025-03-08 16:05:32 -0600

    Evitar la maldicion del Hola Mundo.

---
+++ teoria.dart
@@ -0,0 +1,14 @@
+//Una funcion es un bloque de codigo que nos permite realizar operaciones.
+//Sin el main no funciona.
+void main() {
+
+  /* 
+  34
+  34
+  34
+  34
+  34
+  */
+  
+  print("Hola mundo desde Dart");
+}
