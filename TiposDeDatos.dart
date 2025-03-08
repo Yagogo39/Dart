61ce43f9954ab276cb5ad6b09788a08e22ef2083
Yagogoo39 <118009609+Yagogo39@users.noreply.github.com>
2025-03-08 16:24:41 -0600

    Tipos de datos

---
+++ TiposDeDatos.dart
@@ -0,0 +1,25 @@
+void main(List<String> args) {
+  
+  String nombre = "Diego Carlos Marcial";
+  nombre = "DiegoGo";
+  print(nombre);
+
+  int numero = 1;
+  print(numero);
+
+  double decimal = 1.5;
+  print(decimal);
+
+
+  bool condicion = true;
+  print(condicion);
+
+  dynamic dinamico = 1;
+  dinamico = "Hola";
+  dinamico = true;
+  dinamico = "Hola";
+  print(dinamico);
+
+  var dato = false;
+  print(dato);
+}
