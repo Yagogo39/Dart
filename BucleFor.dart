98e1608edc42d59cde8e4d47e1bca8abe6193a56
Yagogoo39 <118009609+Yagogo39@users.noreply.github.com>
2025-03-09 16:59:10 -0600

    Add files via upload

---
+++ BucleFor.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  print("Tablas de multiplicar");
+  var r;
+
+  for (var i = 1; i < 11; i++) {
+    print("\nTabla del $i");
+    for (var j = 1; j < 11; j++) {
+      r = i * j;
+      print("$i*$j=$r");
+    }
+  }
+}
