f3c95df2da6bcc88686aa701d19816de30b06d80
Yagogoo39 <118009609+Yagogo39@users.noreply.github.com>
2025-03-10 18:58:41 -0600

    Add files via upload

---
+++ Excepciones.dart
@@ -0,0 +1,12 @@
+void main(List<String> args) {
+  
+  try {
+    String mensaje = "Hola";
+    print({mensaje[5]});
+    
+  } catch (e) {
+    print(e);
+    print("Error en la capa 8");
+  }
+
+}
