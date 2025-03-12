e4f5080eb5e00ecedc781d6e50d4a691edd559b4
Yagogoo39 <118009609+Yagogo39@users.noreply.github.com>
2025-03-11 19:05:34 -0600

    Add files via upload

---
+++ Funciones.dart
@@ -0,0 +1,10 @@
+void main() {
+  var resultado = suma(34, 6);
+  print(resultado);
+}
+
+//Este metodo es como la maquina.
+//Dentro de los parentesis se colocan los ingredientes.
+int suma(int a, int b){
+  return a+b;
+}
