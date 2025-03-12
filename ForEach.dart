921105b99e131f546ddb74e716253ea447420e3a
Yagogoo39 <118009609+Yagogo39@users.noreply.github.com>
2025-03-11 19:04:56 -0600

    Add files via upload

---
+++ ForEach.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  
+  var lista=[1,2,3];
+
+/*
+  FOR NORMAL
+  for(var item in lista){
+    print(item);
+  }
+*/
+
+//FOR-EACH
+  lista.forEach((item) =>print(item));
+}
