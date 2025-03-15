54abf47d92cc068088894f768e4c75a2b7e8ef13
Yagogoo39 <118009609+Yagogo39@users.noreply.github.com>
2025-03-14 20:30:00 -0600

    Add files via upload

---
+++ OperadoresLogRelacCondic.dart
@@ -0,0 +1,29 @@
+void main(List<String> args) {
+
+  //Relacionales
+  /*
+  ==
+  !=
+  <
+  <=
+  >
+  >=
+  */
+
+  //Logicos
+  /*
+  ||
+  &&
+  !
+  */
+
+  var precio = 34.5;
+    if(precio == 34.5 || precio == 0){
+    print("Precio Promedio");
+  }else if(precio > 34.5){
+    print("Precio mayor al promedio");
+  }else{
+    print("Precio menor al promedio");
+  }
+
+}
