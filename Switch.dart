0bddb651e8c4b3f0cb69ce18bdce3b8ee536e4a2
Yagogoo39 <118009609+Yagogo39@users.noreply.github.com>
2025-03-16 14:26:32 -0600

    Add files via upload

---
+++ Switch.dart
@@ -0,0 +1,15 @@
+void main(List<String> args) {
+  
+  var item = "Otra cosa";
+
+  switch(item){
+    case "Abierto":
+      print("Caso 1");
+      break;
+    case "Cerrado": 
+      print("Caso 2");
+      break;
+    default:
+      print("Ningun caso");
+  }
+}
